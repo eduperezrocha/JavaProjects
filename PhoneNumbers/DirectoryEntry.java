d966d1b08da9c1b149eeba05e6eb86627791b16a
Eduardo Perez Rocha <64432367+eduperezrocha@users.noreply.github.com>
2023-11-16 16:09:49 +0900

    Add files via upload

---
+++ PhoneNumbers/DirectoryEntry.java
@@ -0,0 +1,26 @@
+public class DirectoryEntry {
+    private String name;
+    private String number;
+    
+
+    public DirectoryEntry(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
